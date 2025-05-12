cc4dd99c529c7dcc0b59beea99e0d106e0fd0493
amonuddin <amonuddinm@gmail.com>
2025-05-12 14:52:48 +0530

    sent

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BookCard from './components/bookcard';
 
 function App() {
   const books = [
@@ -34,7 +35,20 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return <>{<div>
+    <h1>Digital Library</h1>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          image={book.image}
+          name={book.name}
+          genre={book.genre}
+          author={book.author}
+        />
+      ))}
+    </div>
+  </div>}</>;
 }
 
 export default App;
